933d76a7224e06031d5897ab62b4bebfd74b464e
Eduards Gruberts <eduards.gruberts@gmail.com>
2026-02-10 13:25:52 +0200

    Well, let's try flake

--- flake.nix
+++ flake.nix
@@ -24,6 +24,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs; };
             home-manager.users.eg = import ./home/eg.nix;
           }
         ];
