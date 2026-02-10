e3fde0ec9f6906416ccd7409c7783cc85fb6964f
Eduards Gruberts <eduards.gruberts@gmail.com>
2026-02-10 13:17:32 +0200

    Well, let's try flake

--- flake.nix
+++ flake.nix
@@ -2,33 +2,33 @@
   description = "NixOS";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+  let
 
-      nixosConfigurations.l14 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
+    specialArgs = { inherit inputs; };
+  in {
+    nixosConfigurations = {
 
+      l14 = nixpkgs.lib.nixosSystem {
+        inherit specialArgs;
         modules = [
-          ./configuration.nix
-          ./hosts/l14.nix
-
-
+          ./hosts/l14/configuration.nix
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-
-            home-manager.users.eg = import ./home.nix;
+            home-manager.users.eg = import ./common/home.nix;
           }
         ];
       };
+
     };
-  }
+  };
+}
