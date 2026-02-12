c06c7464a275ba6725492ae7b8a450207db32c57
Edaurds Gruberts <edaurds.gruberts@gmail.com>
2026-02-12 22:04:32 +0200

    update zed

--- home/eg.nix
+++ home/eg.nix
@@ -77,149 +77,172 @@
         "Window to Previous Screen" = "Meta+Left";
         "Maximize Window" = "Meta+F";
       };
+      edit = {
+        "Copy" = "Ctrl+C";
+        "Cut" = "Ctrl+X";
+        "Paste" = "Ctrl+V";
+      }
     };
   };
 
 
-programs.zed-editor = {
-  enable = true;
+  programs.zed-editor = {
+    enable = true;
 
-  # This populates the userSettings "auto_install_extensions"
-  extensions = [ "nix" "toml" "make" "PHP" "Java"];
+    # This populates the userSettings "auto_install_extensions"
+    extensions = [ "nix" "toml" "make" "PHP" "Java"];
 
 
-  # Everything inside of these brackets are Zed options
-  userSettings = {
+    # Everything inside of these brackets are Zed options
+    userSettings = {
 
-    preview_tabs = {
-      enabled = false;
-      enable_preview_from_file_finder = true;
-      enable_keep_preview_on_code_navigation = true;
-    };
-
-    assistant = {
-      enabled = true;
-      version = "2";
-      default_open_ai_model = null;
-
-      # Provider options:
-      # - zed.dev models (claude-3-5-sonnet-latest) requires GitHub connected
-      # - anthropic models (claude-3-5-sonnet-latest, claude-3-haiku-latest, claude-3-opus-latest) requires API_KEY
-      # - copilot_chat models (gpt-4o, gpt-4, gpt-3.5-turbo, o1-preview) requires GitHub connected
-      default_model = {
-        provider = "zed.dev";
-        model = "claude-3-5-sonnet-latest";
+      preview_tabs = {
+        enabled = false;
+        enable_preview_from_file_finder = true;
+        enable_keep_preview_on_code_navigation = true;
       };
 
-      # inline_alternatives = [
-      #   {
-      #     provider = "copilot_chat";
-      #     model = "gpt-3.5-turbo";
-      #   }
-      # ];
-    };
+      assistant = {
+        enabled = true;
+        version = "2";
+        default_open_ai_model = null;
 
-#    node = {
-#      path = lib.getExe pkgs.nodejs;
-#      npm_path = lib.getExe' pkgs.nodejs "npm";
-#    };
-
-    hour_format = "hour24";
-    auto_update = false;
-
-    terminal = {
-      alternate_scroll = "off";
-      blinking = "off";
-      copy_on_select = false;
-      dock = "bottom";
-      detect_venv = {
-        on = {
-          directories = [ ".env" "env" ".venv" "venv" ];
-          activate_script = "default";
+        # Provider options:
+        # - zed.dev models (claude-3-5-sonnet-latest) requires GitHub connected
+        # - anthropic models (claude-3-5-sonnet-latest, claude-3-haiku-latest, claude-3-opus-latest) requires API_KEY
+        # - copilot_chat models (gpt-4o, gpt-4, gpt-3.5-turbo, o1-preview) requires GitHub connected
+        default_model = {
+          provider = "zed.dev";
+          model = "claude-3-5-sonnet-latest";
         };
-      };
-      env = {
-        TERM = "alacritty";
-      };
-      font_family = "FiraCode Nerd Font";
-      font_features = null;
-      font_size = null;
-      line_height = "comfortable";
-      option_as_meta = false;
-      button = false;
-      shell = "system";
-      # shell = {
-      #   program = "zsh";
-      # };
-      toolbar = {
-        title = true;
-      };
-      working_directory = "current_project_directory";
-    };
 
-    lsp = {
-      rust-analyzer = {
-        binary = {
-          # path = lib.getExe pkgs.rust-analyzer;
-          path_lookup = true;
-        };
+        # inline_alternatives = [
+        #   {
+        #     provider = "copilot_chat";
+        #     model = "gpt-3.5-turbo";
+        #   }
+        # ];
       };
 
-      nix = {
-        binary = {
-          path_lookup = true;
+      userKeymaps = [
+        {
+          context = "Workspace";
+          bindings = {
+            #"ctrl-shift-t" = "workspace::NewTerminal";
+            #"copy": "editor::Copy",
+            "ctrl-c": "editor::Copy",
+            "ctrl-v": "editor::Paste",
+            "ctrl-x": "editor::Cut",
+            "ctrl-s" = "workspace::Save";
+            "ctrl-shift-f" = "workspace::SearchInWorkspace";
+          };
+        }
+      ];
+
+
+      mutableUserKeymaps = false;
+
+  #    node = {
+  #      path = lib.getExe pkgs.nodejs;
+  #      npm_path = lib.getExe' pkgs.nodejs "npm";
+  #    };
+
+      hour_format = "hour24";
+      auto_update = false;
+
+      terminal = {
+        alternate_scroll = "off";
+        blinking = "off";
+        copy_on_select = false;
+        dock = "bottom";
+        detect_venv = {
+          on = {
+            directories = [ ".env" "env" ".venv" "venv" ];
+            activate_script = "default";
+          };
         };
+        env = {
+          TERM = "alacritty";
+        };
+        font_family = "FiraCode Nerd Font";
+        font_features = null;
+        font_size = null;
+        line_height = "comfortable";
+        option_as_meta = false;
+        button = false;
+        shell = "system";
+        # shell = {
+        #   program = "zsh";
+        # };
+        toolbar = {
+          title = true;
+        };
+        working_directory = "current_project_directory";
       };
 
-      elixir-ls = {
-        binary = {
-          path_lookup = true;
+      lsp = {
+        rust-analyzer = {
+          binary = {
+            # path = lib.getExe pkgs.rust-analyzer;
+            path_lookup = true;
+          };
         };
-        settings = {
-          dialyzerEnabled = true;
-        };
-      };
-    };
 
-    languages = {
-      "Elixir" = {
-        language_servers = [ "!lexical" "elixir-ls" "!next-ls" ];
-        format_on_save = {
-          external = {
-            command = "mix";
-            arguments = [ "format" "--stdin-filename" "{buffer_path}" "-" ];
+        nix = {
+          binary = {
+            path_lookup = true;
+          };
+        };
+
+        elixir-ls = {
+          binary = {
+            path_lookup = true;
+          };
+          settings = {
+            dialyzerEnabled = true;
           };
         };
       };
 
-      "HEEX" = {
-        language_servers = [ "!lexical" "elixir-ls" "!next-ls" ];
-        format_on_save = {
-          external = {
-            command = "mix";
-            arguments = [ "format" "--stdin-filename" "{buffer_path}" "-" ];
+      languages = {
+        # "Elixir" = {
+        #   language_servers = [ "!lexical" "elixir-ls" "!next-ls" ];
+        #   format_on_save = {
+        #     external = {
+        #       command = "mix";
+        #       arguments = [ "format" "--stdin-filename" "{buffer_path}" "-" ];
+        #     };
+        #   };
+        # };
+
+        "HEEX" = {
+          language_servers = [ "!lexical" "elixir-ls" "!next-ls" ];
+          format_on_save = {
+            external = {
+              command = "mix";
+              arguments = [ "format" "--stdin-filename" "{buffer_path}" "-" ];
+            };
           };
         };
       };
+
+      vim_mode = true;
+
+      # Tell Zed to use direnv and direnv can use a flake.nix environment
+      load_direnv = "shell_hook";
+      base_keymap = "VSCode";
+
+      theme = {
+        mode = "system";
+        light = "Gruvbox Dark";
+        dark = "One Dark";
+      };
+
+      show_whitespaces = "all";
+      ui_font_size = 16;
+      buffer_font_size = 16;
     };
-
-    vim_mode = true;
-
-    # Tell Zed to use direnv and direnv can use a flake.nix environment
-    load_direnv = "shell_hook";
-    base_keymap = "VSCode";
-
-    theme = {
-      mode = "system";
-      light = "Gruvbox Dark";
-      dark = "One Dark";
-    };
-
-    show_whitespaces = "all";
-    ui_font_size = 16;
-    buffer_font_size = 16;
   };
-};
 
 
   # xdg.configFile."dolphinrc" = {
