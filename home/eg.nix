1ef1b32a7ad20395daa3e7ab6cff394bdffb2902
Edaurds Gruberts <edaurds.gruberts@gmail.com>
2026-02-14 12:48:38 +0200

    update klipper

--- home/eg.nix
+++ home/eg.nix
@@ -75,11 +75,10 @@
         "Show Desktop Grid" = "Meta+G";
         "Window to Next Screen" = "Meta+Left";
         "Window to Previous Screen" = "Meta+Left";
-      };
-      "Window Management" = {
         "Maximize Window" = "Meta+F";
       };
-      "Edit" = {
+
+      klipper = {
         "Copy" = "Ctrl+C";
         "Cut" = "Ctrl+X";
         "Paste" = "Ctrl+V";
