2572fab0e2989c768832c788cd59cf4de4942e70
Edaurds Gruberts <edaurds.gruberts@gmail.com>
2026-02-15 18:07:56 +0200

    add libreoffice

--- tools/libreoffice.nix
+++ tools/libreoffice.nix
@@ -3,7 +3,12 @@
 {
   environment.systemPackages = with pkgs; [
     libreoffice-qt-fresh
+    adwaita-icon-theme
+    hicolor-icon-theme
   ];
 
+  # environment.sessionVariables = {
+  #   QT_QPA_PLATFORMTHEME = "qt5ct";
+  # };
 
 }
