e5eb9dfac717aa5bc1b74b8c9abb88d211af339b
Edaurds Gruberts <edaurds.gruberts@gmail.com>
2026-02-15 18:33:54 +0200

    add libreoffice

--- tools/libreoffice.nix
+++ tools/libreoffice.nix
@@ -5,10 +5,13 @@
     libreoffice-qt-fresh
     adwaita-icon-theme
     hicolor-icon-theme
+
+    kdePackages.qt6ct
+    kdePackages.breeze-icons
   ];
 
-  # environment.sessionVariables = {
-  #   QT_QPA_PLATFORMTHEME = "qt5ct";
-  # };
+  environment.sessionVariables = {
+    QT_QPA_PLATFORMTHEME = "qt6ct";
+  };
 
 }
