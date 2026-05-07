80774ce8671c27d3fb9edbf3a96b15eb88a0cde2
Edaurds Gruberts <edaurds.gruberts@gmail.com>
2026-05-07 17:35:48 +0300

    update environment

--- tools/android.nix
+++ tools/android.nix
@@ -78,6 +78,8 @@
 
       # ANDROID_HOME = "$HOME/android/sdk";
       # ANDROID_SDK_ROOT = "$HOME/android/sdk";
+      ANDROID_HOME = "${pkgs.androidenv.androidPkgs.androidsdk}/libexec/android-sdk";
+      ANDROID_SDK_ROOT = "${pkgs.androidenv.androidPkgs.androidsdk}/libexec/android-sdk";
       ANDROID_AVD_HOME = "$HOME/.config/.android/avd";
 
       # PATH = [
