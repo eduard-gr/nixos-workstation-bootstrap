b1bd5670de53feb9fd5f881a4d1d2baeeb7294a9
Edaurds Gruberts <edaurds.gruberts@gmail.com>
2026-05-07 14:44:55 +0300

    update environment

--- tools/android.nix
+++ tools/android.nix
@@ -7,6 +7,33 @@
   #list devices
   #services.udev.packages = [ pkgs.android-udev-rules ];
 
+  #enable dynamic libs
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+      stdenv.cc.cc
+      libGL
+      glib
+      nss
+      nspr
+      expat
+      fontconfig
+      freetype
+      dbus
+      udev
+      libvirt
+      xorg.libX11
+      xorg.libXcursor
+      xorg.libXcomposite
+      xorg.libXdamage
+      xorg.libXext
+      xorg.libXfixes
+      xorg.libXi
+      xorg.libXrender
+      xorg.libXtst
+      libpulseaudio
+      libuuid
+    ];
+
   environment.systemPackages = with pkgs; [
     android-tools
     android-studio-full
