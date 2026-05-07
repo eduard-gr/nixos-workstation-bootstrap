d9cacc582cbe86d66a1c5cc5af23453763f5455c
Edaurds Gruberts <edaurds.gruberts@gmail.com>
2026-05-07 18:01:38 +0300

    update environment

--- tools/android.nix
+++ tools/android.nix
@@ -1,65 +1,59 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
-  #systemd 258 no longer has any effect
-  #programs.adb.enable = true;
-
-  #list devices
-  #services.udev.packages = [ pkgs.android-udev-rules ];
   nixpkgs.config.android_sdk.accept_license = true;
 
-  #enable dynamic libs
-  #programs.nix-ld.enable = true;
-  # programs.nix-ld.libraries = with pkgs; [
-  #     stdenv.cc.cc
-  #     libGL
-  #     glib
-  #     nss
-  #     nspr
-  #     expat
-  #     fontconfig
-  #     freetype
-  #     dbus
-  #     udev
-  #     libvirt
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+      stdenv.cc.cc
+      libGL
+      glib
+      nss
+      nspr
+      expat
+      fontconfig
+      freetype
+      dbus
+      udev
+      libvirt
 
-  #     alsa-lib
-  #     libx11
-  #     libxcursor
-  #     libxcomposite
-  #     libxdamage
-  #     libxext
-  #     libxfixes
-  #     libxi
-  #     libxrender
-  #     libxtst
-  #     libxcb
-  #     libXScrnSaver
-  #     libxkbfile
-  #     libXinerama
-  #     libXrandr
-  #     libXres
-  #     libXv
-  #     libxkbcommon
-  #     libxcb-cursor
-  #     xcb-util-cursor
+      alsa-lib
+      libx11
+      libxcursor
+      libxcomposite
+      libxdamage
+      libxext
+      libxfixes
+      libxi
+      libxrender
+      libxtst
+      libxcb
+      libXScrnSaver
+      libxkbfile
+      libXinerama
+      libXrandr
+      libXres
+      libXv
+      libxkbcommon
+      libxcb-cursor
+      xcb-util-cursor
 
-  #     libbsd
-  #     atk
-  #     at-spi2-atk
-  #     cups
-  #     libdrm
+      libbsd
+      atk
+      at-spi2-atk
+      cups
+      libdrm
 
-  #     libpulseaudio
-  #     libuuid
-  #     libpng
-  #     libjpeg
-  #     zlib
-  #     mesa
-  #     libdrm
+      libpulseaudio
+      libuuid
+      libpng
+      libjpeg
+      zlib
+      mesa
+      libdrm
 
-  #     vulkan-loader
-  #   ];
+      vulkan-loader
+    ];
 
   environment.systemPackages = with pkgs; [
     android-tools
@@ -79,15 +73,8 @@
       # ANDROID_HOME = "$HOME/android/sdk";
 
       ANDROID_HOME = "${pkgs.androidenv.androidPkgs.androidsdk}/libexec/android-sdk";
-      #ANDROID_SDK_ROOT = "${pkgs.androidenv.androidPkgs.androidsdk}/libexec/android-sdk";
-      ANDROID_SDK_ROOT = "$HOME/android/sdk";
+      ANDROID_SDK_ROOT = "${pkgs.androidenv.androidPkgs.androidsdk}/libexec/android-sdk";
       ANDROID_AVD_HOME = "$HOME/.config/.android/avd";
-
-      # PATH = [
-      #   "$HOME/android/sdk/platform-tools"
-      #   "$HOME/android/sdk/emulator"
-      #   "$HOME/android/sdk/cmdline-tools/latest/bin"
-      # ];
   };
 
   # environment.shellAliases = {
